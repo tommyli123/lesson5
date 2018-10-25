7583a6339a36db8199285e8a60fc6e04b744fdc3
Tommy Li <tommy.li@autodesk.com>
2018-10-25 15:20:37 -0700

    test

--- test_functions.py
+++ test_functions.py
@@ -4,7 +4,7 @@ from functions import *
 
 class TestFunctions(unittest.TestCase):
     """ 
-    """ Test add_values (add new comment)
+    """ Test add_values (add new comment 3)
     """
     def setUp(self):
         self.value1 = 10
